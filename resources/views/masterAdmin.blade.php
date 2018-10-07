2c1713d2a952a31fba55898f923c1faa3049d6da
Adan Talavera <adan_talavera@hotmail.com>
2018-10-07 03:16:05 -0600

    Login y Sign in

--- resources/views/masterAdmin.blade.php
+++ resources/views/masterAdmin.blade.php
@@ -63,7 +63,19 @@
 			</div>
   		</div>
 	</nav>
+	@if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
 
+                        @if (Request::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
 
 	<div class="panel-heading">
 		<div class="content w3-container">
--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,35 @@
 
 Route::get('master', function () {
     return view('masterAdmin');
-});
+});//->middleware('auth.basic');
 
  Route::get('/',function () {
-     return view('Prueba.login');
- });
+     return view('auth/login');
+ });//->middleware('auth.basic');
 
 Route::get('prueba', function () {
     return view('Prueba/index') ;
-});
+});//->middleware('auth.basic');
 
 Route::get('paciente', function () {
     return view('Prueba/paciente') ;
-});
+});//->middleware('auth.basic');
 
 Route::post('login', 'Auth\LoginController@login')-> name('login');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
