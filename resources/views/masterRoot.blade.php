08c3fd95334f599c9255dc21a715b493aeba69cb
YerickRamirez <yerick.ramirez@ucrso.info>
2018-11-16 17:15:21 -0600

    logo

--- resources/views/masterRoot.blade.php
+++ resources/views/masterRoot.blade.php
@@ -74,7 +74,7 @@
 		      			<a class="border" href="cuentas">Cuentas<span class="glyphicon glyphicon-user right-aling-glyphicon"></a>
 		    		</li>
 					<li>
-		      			<a class="border" href="{{ route('especialistas.index') }}">Especialistas<span class="glyphicon glyphicon-education right-aling-glyphicon"></a>
+		      			<a class="border" href="{{ route('especialistas.index') }}">Especialistas<span class="glyphicon glyphicon-education right-aling-glyphicon-e"></a>
 		    		</li>
 		    		<li>
 		      			<a class="border" href="{{ route('Admin.horarios') }}">Horarios<span class="glyphicon glyphicon glyphicon-wrench right-aling-glyphicon"></a>
