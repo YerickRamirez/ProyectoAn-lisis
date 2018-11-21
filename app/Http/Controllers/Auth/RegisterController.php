6e22480fa10034215fdaf1dbe9d11a6aac4bb855
Adan Talavera <adan_talavera@hotmail.com>
2018-11-21 10:24:18 -0600

    123

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,7 +74,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-
        Mail::to($data['email'])->send(new Confirmacion($data['name']));
         $tipo = 'paciente';
 
