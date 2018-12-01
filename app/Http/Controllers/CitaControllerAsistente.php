5f5cb43ec36c3f577894592b26c9c65a5ae29d2d
Seney Alvarado Carvajal <seney.alvarado@ucrso.info>
2018-12-01 14:44:27 -0600

    Citas ya no contemplan active_flag.

--- app/Http/Controllers/CitaControllerAsistente.php
+++ app/Http/Controllers/CitaControllerAsistente.php
@@ -51,11 +51,10 @@ class CitaControllerAsistente extends Controller
 	{
 		$citas = $cita = DB::table('citas')
 		->join('telefonos', 'citas.paciente_id', '=', 'telefonos.paciente_id')
-		->where('citas.active_flag', '!=',0)
+		//->where('citas.active_flag', '!=',0)
 		->where('citas.estado_cita_id', '!=',3)
-		->where('telefonos.active_flag', 1)
+		//->where('telefonos.active_flag', 1)
 		->join('pacientes', 'citas.paciente_id', '=', 'pacientes.id')
-		->where('pacientes.active_flag', 1)
 		->select('citas.id as id_cita',
 			'citas.fecha_cita',
 			'citas.estado_cita_id', 
@@ -87,16 +86,16 @@ class CitaControllerAsistente extends Controller
 		->whereDate('fecha_cita', '<=', $fechaFinCarbon)
 		->where('recintos.id', $ID_Recinto)
 		->join('recintos', 'citas.recinto_id', '=', 'recintos.id')
-		->where('recintos.active_flag', '!=',0)
+		//->where('recintos.active_flag', '!=',0)
 		->join('telefonos', 'citas.paciente_id', '=', 'telefonos.paciente_id')
-		->where('citas.active_flag', '!=',0)
+		//->where('citas.active_flag', '!=',0)
 		->where('citas.estado_cita_id', $estado)
-		->where('telefonos.active_flag', 1)
+		//->where('telefonos.active_flag', 1)
 		->join('pacientes', 'citas.paciente_id', '=', 'pacientes.id')
 		->join('especialistas', 'citas.especialista_id', '=', 'especialistas.id')
-		->where('especialistas.active_flag', 1)
+		//->where('especialistas.active_flag', 1)
 		->join('servicios', 'citas.servicio_id', '=', 'servicios.id')
-		->where('servicios.active_flag', 1)
+		//->where('servicios.active_flag', 1)
 		->select('citas.id as id_cita',
 			'citas.fecha_cita',
 			'citas.estado_cita_id', 
@@ -127,17 +126,17 @@ class CitaControllerAsistente extends Controller
 		->whereDate('fecha_cita', '<=', $fechaFinCarbon)
 		->where('recintos.id', $ID_Recinto)
 		->join('recintos', 'citas.recinto_id', '=', 'recintos.id')
-		->where('recintos.active_flag', '!=',0)
+		//->where('recintos.active_flag', '!=',0)
 		->join('telefonos', 'citas.paciente_id', '=', 'telefonos.paciente_id')
-		->where('citas.active_flag', '!=',0)
+		//->where('citas.active_flag', '!=',0)
 		->where('citas.estado_cita_id', '!=',3)
 		->where('citas.estado_cita_id', '!=',4)
-		->where('telefonos.active_flag', 1)
+		//->where('telefonos.active_flag', 1)
 		->join('pacientes', 'citas.paciente_id', '=', 'pacientes.id')
 		->join('especialistas', 'citas.especialista_id', '=', 'especialistas.id')
-		->where('especialistas.active_flag', 1)
+		//->where('especialistas.active_flag', 1)
 		->join('servicios', 'citas.servicio_id', '=', 'servicios.id')
-		->where('servicios.active_flag', 1)
+		//->where('servicios.active_flag', 1)
 		->select('citas.id as id_cita',
 			'citas.fecha_cita',
 			'citas.estado_cita_id', 
@@ -172,16 +171,16 @@ class CitaControllerAsistente extends Controller
 		->whereDate('fecha_cita', '>=', $fechaInicioCarbon)
 		->where('recintos.id', $ID_Recinto)
 		->join('recintos', 'citas.recinto_id', '=', 'recintos.id')
-		->where('recintos.active_flag', '!=',0)
+		//->where('recintos.active_flag', '!=',0)
 		->join('telefonos', 'citas.paciente_id', '=', 'telefonos.paciente_id')
-		->where('citas.active_flag', '!=',0)
+		//->where('citas.active_flag', '!=',0)
 		->where('citas.estado_cita_id', $estado)
-		->where('telefonos.active_flag', 1)
+		//->where('telefonos.active_flag', 1)
 		->join('pacientes', 'citas.paciente_id', '=', 'pacientes.id')
 		->join('especialistas', 'citas.especialista_id', '=', 'especialistas.id')
-		->where('especialistas.active_flag', 1)
+		//->where('especialistas.active_flag', 1)
 		->join('servicios', 'citas.servicio_id', '=', 'servicios.id')
-		->where('servicios.active_flag', 1)
+		//->where('servicios.active_flag', 1)
 		->select('citas.id as id_cita',
 			'citas.fecha_cita',
 			'citas.estado_cita_id', 
@@ -211,17 +210,17 @@ class CitaControllerAsistente extends Controller
 		->whereDate('fecha_cita', '>=', $fechaInicioCarbon)
 		->where('recintos.id', $ID_Recinto)
 		->join('recintos', 'citas.recinto_id', '=', 'recintos.id')
-		->where('recintos.active_flag', '!=',0)
+		//->where('recintos.active_flag', '!=',0)
 		->join('telefonos', 'citas.paciente_id', '=', 'telefonos.paciente_id')
-		->where('citas.active_flag', '!=',0)
+		//->where('citas.active_flag', '!=',0)
 		->where('citas.estado_cita_id', '!=',3)
 		->where('citas.estado_cita_id', '!=',4)
-		->where('telefonos.active_flag', 1)
+		//->where('telefonos.active_flag', 1)
 		->join('pacientes', 'citas.paciente_id', '=', 'pacientes.id')
 		->join('especialistas', 'citas.especialista_id', '=', 'especialistas.id')
-		->where('especialistas.active_flag', 1)
+		//->where('especialistas.active_flag', 1)
 		->join('servicios', 'citas.servicio_id', '=', 'servicios.id')
-		->where('servicios.active_flag', 1)
+		//->where('servicios.active_flag', 1)
 		->select('citas.id as id_cita',
 			'citas.fecha_cita',
 			'citas.estado_cita_id', 
@@ -251,17 +250,16 @@ class CitaControllerAsistente extends Controller
 			$citas = $cita = DB::table('citas')
 			->where('recintos.id', $ID_Recinto)
 			->join('recintos', 'citas.recinto_id', '=', 'recintos.id')
-			->where('recintos.active_flag', '!=',0)
+			//->where('recintos.active_flag', '!=',0)
 			->join('telefonos', 'citas.paciente_id', '=', 'telefonos.paciente_id')
-			->where('citas.active_flag', '!=',0)
+			//->where('citas.active_flag', '!=',0)
 			->where('citas.estado_cita_id', $estado)
-			->where('telefonos.active_flag', 1)
+			//->where('telefonos.active_flag', 1)
 			->join('pacientes', 'citas.paciente_id', '=', 'pacientes.id')
-			->where('pacientes.active_flag', 1)
 			->join('especialistas', 'citas.especialista_id', '=', 'especialistas.id')
-			->where('especialistas.active_flag', 1)
+			//->where('especialistas.active_flag', 1)
 			->join('servicios', 'citas.servicio_id', '=', 'servicios.id')
-			->where('servicios.active_flag', 1)
+			//->where('servicios.active_flag', 1)
 			->select('citas.id as id_cita',
 				'citas.fecha_cita',
 				'citas.estado_cita_id', 
@@ -289,18 +287,17 @@ class CitaControllerAsistente extends Controller
 			$citas = $cita = DB::table('citas')
 			->where('recintos.id', $ID_Recinto)
 			->join('recintos', 'citas.recinto_id', '=', 'recintos.id')
-			->where('recintos.active_flag', '!=',0)
+			//->where('recintos.active_flag', '!=',0)
 			->join('telefonos', 'citas.paciente_id', '=', 'telefonos.paciente_id')
-			->where('citas.active_flag', '!=',0)
+			//->where('citas.active_flag', '!=',0)
 			->where('citas.estado_cita_id', '!=',3)
 			->where('citas.estado_cita_id', '!=',4)
-			->where('telefonos.active_flag', 1)
+			//->where('telefonos.active_flag', 1)
 			->join('pacientes', 'citas.paciente_id', '=', 'pacientes.id')
-			->where('pacientes.active_flag', 1)
 			->join('especialistas', 'citas.especialista_id', '=', 'especialistas.id')
-			->where('especialistas.active_flag', 1)
+			//->where('especialistas.active_flag', 1)
 			->join('servicios', 'citas.servicio_id', '=', 'servicios.id')
-			->where('servicios.active_flag', 1)
+			//->where('servicios.active_flag', 1)
 			->select('citas.id as id_cita',
 				'citas.fecha_cita',
 				'citas.estado_cita_id', 
@@ -484,6 +481,7 @@ class CitaControllerAsistente extends Controller
 			}
 		}
 
+		/*
 			public function reprogramar(Cita $cita)
 			{
 			$citas = DB::table('citas')
@@ -507,7 +505,7 @@ class CitaControllerAsistente extends Controller
 				//return "hola";
 				return redirect()->route('asistente.index');
 		
-			}
+			}*/
 
 	/**
 	 * Display the specified resource.
