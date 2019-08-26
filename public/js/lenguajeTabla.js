c60e5069f26d21154b0c77e3887eba62227ebf06
Seney Alvarado Carvajal <seney.alvarado@ucrso.info>
2019-08-26 10:08:19 -0600

    Prueba cambio de colores, nada relevante. Se agregó un archivo htaccess solo

--- public/js/lenguajeTabla.js
+++ public/js/lenguajeTabla.js
@@ -6,3 +6,24 @@ $('#tablaDatos').DataTable(
  stateSave: true,
  "ordering": false,    
     } );
+
+    //No borrar, prueba cambio colores
+    /*$('#tablaDatos').DataTable(
+     {
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+} ,
+ stateSave: true,
+ "ordering": false,    
+ "rowCallback": function( row, data, index ) {
+     //alert(data[2]);
+         if ( data[2] == "Especialista" )
+    {
+        $('td', row).css('background-color', 'Red');
+        //formatStyle('Correo',  color = 'red', backgroundColor = 'orange', fontWeight = 'bold');
+    }
+    else if ( data[2] == "4" )
+    {
+        $('td', row).css('background-color', 'Orange');
+    }
+    }} );*/
